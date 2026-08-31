b2ecc0df00bacc8157e88bba68e04e5557d9e8fd
Harsh Kumar Singh <harshh1505@gmail.com>
2026-08-31 15:44:48 +0530

    seo: update sitemap URL in robots.ts to point to canonical .in sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/dashboard', '/auth/'],
     },
-    sitemap: 'https://promoteducation.com/sitemap.xml',
+    sitemap: 'https://promoteeducation.in/sitemap.xml',
   }
 }
